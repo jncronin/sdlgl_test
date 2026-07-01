47963eb17833b3428842ca3ab44cc5eac0bb56bc
John Cronin <jncronin@tysos.org>
2026-07-01 13:42:33 +0100

    basic triangle test

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <SDL2/SDL.h>
+#include <GL/gl.h>
+
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        return 1;
+    }
+
+    auto wnd = SDL_CreateWindow("sdlgl_test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        800, 480, SDL_WINDOW_OPENGL);
+
+    auto ctx = SDL_GL_CreateContext(wnd);
+
+    bool running = true;
+    float angle = 0.0f;
+    while(running)
+    {
+        SDL_Event e;
+        while(SDL_PollEvent(&e))
+        {
+            if(e.type == SDL_QUIT)
+            {
+                running = false;
+                break;
+            }
+        }
+
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glLoadIdentity();
+        glRotatef(angle, 0.0f, 0.0f, 1.0f);
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(0.0f, 0.5f, 0.0f);
+        glVertex3f(-0.5f, -0.5f, 0.0f);
+        glVertex3f(0.5f, -0.5f, 0.0f);
+        glEnd();
+
+        SDL_GL_SwapWindow(wnd);
+
+        angle += 0.01f;
+    }
+
+
+    SDL_GL_DeleteContext(ctx);
+    SDL_DestroyWindow(wnd);
+    SDL_Quit();
+
+    return 0;
+}
