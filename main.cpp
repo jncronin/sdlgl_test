e699e4b893c3827e8f19b3315cd4ab8541ad58be
John Cronin <jncronin@tysos.org>
2026-07-02 13:54:02 +0100

    fix missing glViewport call, tidy up code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <SDL2/SDL.h>
+#define GL_GLEXT_PROTOTYPES
 #include <GL/gl.h>
+#include <GL/glext.h>
+
+const int w = 800;
+const int h = 480;
 
 int main()
 {
@@ -7,8 +12,15 @@ int main()
         return 1;
     }
 
-    auto wnd = SDL_CreateWindow("sdlgl_test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        800, 480, SDL_WINDOW_OPENGL);
+    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
+    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+    //SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 0);
+    //SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0);
+
+
+    auto wnd = SDL_CreateWindow("sdlgl_test", 0, 0,
+        w, h, SDL_WINDOW_OPENGL);
     if(!wnd)
     {
         fprintf(stderr, "SDL_CreateWindow failed\n");
@@ -24,6 +36,9 @@ int main()
 
     bool running = true;
     float angle = 0.0f;
+
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+
     while(running)
     {
         SDL_Event e;
@@ -43,17 +58,16 @@ int main()
         glRotatef(angle, 0.0f, 0.0f, 1.0f);
 
         glBegin(GL_TRIANGLES);
-        glColor3f(0.0f, 1.0f, 0.0f);
+        glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
         glVertex3f(0.0f, 0.5f, 0.0f);
         glVertex3f(-0.5f, -0.5f, 0.0f);
         glVertex3f(0.5f, -0.5f, 0.0f);
         glEnd();
-
+        
         SDL_GL_SwapWindow(wnd);
 
         angle += 0.01f;
     }
-
 
     SDL_GL_DeleteContext(ctx);
     SDL_DestroyWindow(wnd);
